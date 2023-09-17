56db47b0239201088ef1ab4c388ad9790502ed26
Priyansh Jhalora <79319855+Priyansh-Breach@users.noreply.github.com>
2023-09-17 16:10:58 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,45 +1,34 @@
 # app.py
 
 from flask import Flask, request, jsonify
-import pandas as pd
 import pickle
+import xgboost as xgb
+import pandas as pd
 
 app = Flask(__name__)
 
-# Specify the filename of your model
-model_path = "../xgb_model.pkl"
-
-# Check model loading
-with open(model_path, 'rb') as f:
+# Load the trained XGBoost model
+with open('xgb_model.pkl', 'rb') as f:
     model = pickle.load(f)
-    print("Model loaded successfully.")
 
 @app.route('/')
-def mn():
+def home():
     return "Jai Shree Ram!"
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # try:
-    # Get data from the request as JSON
-    data = request.get_json()
-    print(data)
+    try:
+        # Get data from the request as JSON
+        data = request.get_json()
+        # Perform inference using the loaded model
+        prediction = model.predict(pd.DataFrame([data]))
 
+        # Convert the prediction to the desired format
+        # For example, if the prediction is a NumPy array, you can convert it to a list
+        prediction = prediction.tolist()
 
-    d = pd.DataFrame([data])
-    print(d.dtypes)
+        # Return the prediction as JSON
+        return jsonify({'prediction': prediction})
 
-    # Make predictions
-    print(model.get_booster().feature_names)
-    prediction = model.predict(d)
-    print("Prediction:", prediction)
-
-    # Convert the prediction to the desired format
-    prediction = prediction.tolist()
-
-    # Return the prediction as JSON
-    return jsonify({'prediction': prediction})
-
-    # except Exception as e:
-    #     print("Error:", str(e))
-    #     return jsonify({'error': str(e)})
+    except Exception as e:
+        return jsonify({'error': str(e)})
