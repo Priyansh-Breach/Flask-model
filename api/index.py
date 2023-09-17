31eb8cf981ce9db264e380fdb245712c9319c2b6
Priyansh Jhalora <79319855+Priyansh-Breach@users.noreply.github.com>
2023-09-17 16:40:59 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,38 +1,38 @@
 # app.py
 
 from flask import Flask, request, jsonify
-import pickle
-import pandas as pd
+# import pickle
+# import pandas as pd
 
 app = Flask(__name__)
 
-# Load the trained XGBoost model
-with open('xgb_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# # Load the trained XGBoost model
+# with open('xgb_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
-@app.route('/favicon.ico')
-def favicon():
-    # Return an empty response or an actual favicon.ico file
-    return '', 204
+# @app.route('/favicon.ico')
+# def favicon():
+#     # Return an empty response or an actual favicon.ico file
+#     return '', 204
 
 @app.route('/')
 def home():
     return "Jai Shree Ram!"
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        # Get data from the request as JSON
-        data = request.get_json()
-        # Perform inference using the loaded model
-        prediction = model.predict(pd.DataFrame([data]))
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     try:
+#         # Get data from the request as JSON
+#         data = request.get_json()
+#         # Perform inference using the loaded model
+#         prediction = model.predict(pd.DataFrame([data]))
 
-        # Convert the prediction to the desired format
-        # For example, if the prediction is a NumPy array, you can convert it to a list
-        prediction = prediction.tolist()
+#         # Convert the prediction to the desired format
+#         # For example, if the prediction is a NumPy array, you can convert it to a list
+#         prediction = prediction.tolist()
 
-        # Return the prediction as JSON
-        return jsonify({'prediction': prediction})
+#         # Return the prediction as JSON
+#         return jsonify({'prediction': prediction})
 
-    except Exception as e:
-        return jsonify({'error': str(e)})
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
