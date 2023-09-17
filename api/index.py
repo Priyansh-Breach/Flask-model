6a0e5eb1c360f12f7ef4bd7ab76832abac28d32d
Priyansh Jhalora <79319855+Priyansh-Breach@users.noreply.github.com>
2023-09-17 15:47:35 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,45 @@
-from flask import Flask
+# app.py
+
+from flask import Flask, request, jsonify
+import pandas as pd
+import pickle
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Specify the filename of your model
+model_path = "../xgb_model.pkl"
 
-@app.route('/about')
-def about():
-    return 'About'
+# Check model loading
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+    print("Model loaded successfully.")
+
+@app.route('/')
+def mn():
+    return "Jai Shree Ram!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get data from the request as JSON
+        data = request.get_json()
+        print(data)
+
+
+        d = pd.DataFrame([data])
+        print(d.dtypes)
+
+        # Make predictions
+        print(model.get_booster().feature_names)
+        prediction = model.predict(d)
+        print("Prediction:", prediction)
+
+        # Convert the prediction to the desired format
+        prediction = prediction.tolist()
+
+        # Return the prediction as JSON
+        return jsonify({'prediction': prediction})
+
+    except Exception as e:
+        print("Error:", str(e))
+        return jsonify({'error': str(e)})
